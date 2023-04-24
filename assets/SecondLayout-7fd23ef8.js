197e175d98f054d8af7d6d1ed7d643f736ef3222
주영희 <joo02@juyeonghuiui-MacBookPro.local>
2023-04-24 09:09:33 +0900

    hosting

---
+++ assets/SecondLayout-7fd23ef8.js
@@ -0,0 +1,1 @@
+import{c as e,d as o,y as s,h as t,o as a,R as c}from"./index-a1cb39a7.js";const _={class:"second-layout flex flex-col"},n={class:"flex grow"},i={__name:"SecondLayout",setup(r){return(l,d)=>(a(),e("div",_,[o("div",n,[s(t(c))])]))}};export{i as default};
