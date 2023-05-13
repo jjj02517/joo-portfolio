9d551645fc4c11d992ac9b87df4737e82d12d559
주영희 <joo02@juyeonghuiui-MacBookPro.local>
2023-05-14 08:49:17 +0900

    hosting

---
+++ assets/SecondLayout-dc38f2c1.js
@@ -0,0 +1,1 @@
+import{_ as t,P as s}from"./Footer-3b0faa88.js";import{c as a,b as e,a as c,o as _,e as n}from"./index-1bde508c.js";const r={class:"second-layout flex flex-col"},l={class:"container flex grow mx-auto"},f={__name:"SecondLayout",setup(u){return(i,d)=>{const o=n("RouterView");return _(),a("div",r,[e(t),c("div",l,[e(o,{class:"w-full"})]),e(s)])}}};export{f as default};
